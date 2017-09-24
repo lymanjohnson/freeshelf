a30f86ac65786312feaceb52ab8f0ded2964264e
lymanjohnson <lyman.johnson@gmail.com>
2017-09-24 14:31:07 -0400

    Created controller view model etc for adding new books to db

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,20 @@
 class BooksController < ApplicationController
 
+  def show
+    @book = Book.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:book].inspect
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to @book
   end
 
+  private
+    def book_params
+      params.require(:book).permit(:title,:author,:year,:genre,:description)
+    end
 end
