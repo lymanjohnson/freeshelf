a0b5f24ecfa926844c5303360ce6c23573c5e2bb
lymanjohnson <lyman.johnson@gmail.com>
2017-09-24 16:42:10 -0400

    Added error checking

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   def new
+    @book = Book.new
   end
 
   def create
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   validates :title, presence: true,
                   length: { maximum: 50 }
-  validates :description, presence: true, length: {maximum: 300, minimum: 5}
+  validates :description, length: {maximum: 300}
+  validates :author, presence: true
 end
