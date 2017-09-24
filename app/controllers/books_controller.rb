14981960123caa0a12188c1daa46c700764f6553
lymanjohnson <lyman.johnson@gmail.com>
2017-09-24 13:49:06 -0400

    Added /books/new router and view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,6 @@
 class BooksController < ApplicationController
+
+  def new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :articles
+  resources :books
 
   root 'welcome#index'
 
