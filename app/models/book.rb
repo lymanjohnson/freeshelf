177b054cda239dc177be08003878ec033cf5b561
lymanjohnson <lyman.johnson@gmail.com>
2017-09-24 16:42:02 -0400

    Added error checking

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  validates :title, presence: true,
+                  length: { maximum: 50 }
+  validates :description, presence: true, length: {maximum: 300, minimum: 5}
 end
