06762a230c5bd588fdccbd4a0a89b824cf5acd6b
lymanjohnson <lyman.johnson@gmail.com>
2017-09-24 19:34:02 -0400

    Basic authentication implemented

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,11 @@
 class BooksController < ApplicationController
 
+  http_basic_authenticate_with name: "dhh", password: "secret2", except: [:index, :show]
+
+  def index
+    @articles = Article.all
+  end
+
   def index
     @books = Book.all
   end
